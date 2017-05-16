396c320dc2702ed225a0b7b8cd2ca36f42cd38bc
Katie Nichols <kaylotura@gmail.com>
2017-05-16 16:44:04 -0700

    added charcter tables module and races table

---
+++ src/tables/characterTables.js
@@ -0,0 +1,112 @@
+/**
+ * Created by kaylo on 5/16/2017.
+ */
+
+const raceTable = [
+  // Possible to add 'alternate names.... but seems like a hassle
+  ['Aarakocra'],
+  ['Aasimar Protector', 'Aasimar Scourge', 'Aasimar Fallen'],
+  ['Changeling'],
+  [
+    'Dragonborn: Black Dragon Ancestry',
+    'Dragonborn: Blue Dragon Ancestry',
+    'Dragonborn: Brass Dragon Ancestry',
+    'Dragonborn: Bronze Dragon Ancestry',
+    'Dragonborn: Copper Dragon Ancestry',
+    'Dragonborn: Gold Dragon Ancestry',
+    'Dragonborn: Green Dragon Ancestry',
+    'Dragonborn: Red Dragon Ancestry',
+    'Dragonborn: Silver Dragon Ancestry',
+    'Dragonborn: White Dragon Ancestry'
+  ],
+  ['Gray Dwarf', 'Hill Dwarf', 'Mountain Dwarf'],
+  [
+    'Dark Elf (Drow)',
+    'Eladrin (Elf)',
+    'High Elf', 'Wood Elf',
+    'Zendikar Elf (Tajuru)',
+    'Zendikar Elf (Joraga)',
+    'Zendikar Elf (Mul Daya)'
+  ],
+  ['Firbolg'],
+  ['Air Genasi', 'Earth Genasi', 'Fire Genasi', 'Water Genasi'],
+  ['Forest Gnome', 'Rock Gnome', 'Deep Gnome (Svirfneblin)'],
+  ['Grotag Goblin', 'Lavastep Goblin', 'Tuktuk Goblin', 'Volo Goblin'],
+  ['Goliath'],
+  ['Half-Elf', 'Half Aquatic Elf', 'Half Drow', 'Half Moon Elf', 'Half Sun Elf', 'Half Wood Elf'],
+  ['Half-Orc'],
+  ['Ghostwise Halfling', 'Lightfood Halfling', 'Stout Halfling'],
+  ['Human'],
+  ['Kenku'],
+  ['Kor'],
+  ['Lizardfolk'],
+  ['Cosi Creed Merfolk', 'Emeria Creed Merfolk', 'Ula Creed Merfolk'],
+  // Monstrous Races all packed into one list for weighting reasons
+  [
+    'Minator',
+    'Bugbear',
+    'Hobgoblin',
+    'Kobold',
+    'Orc',
+    'Yuan-ti Pureblood'
+  ],
+  [
+    'Beasthide Shifter',
+    'Cliffwalk Shifter',
+    'Longstride Shifter',
+    'Longtooth Shifter',
+    'Razorclaw Shifter',
+    'Wildhunt Shifter'
+  ],
+  ['Tabaxi'],
+  [
+    'Feral Tiefling',
+    'Devil Tongu Tiefling',
+    'Hellfire Tiefling',
+    'Winged Tiefling',
+    'Infernal Tiefling',
+    'Abyssal Tiefling'
+  ],
+  ['Triton'],
+  ['Zendikar Vampire'],
+  [
+    'Aarakocra Revenant',
+    'Aasimar Revenant',
+    'Changeling Revenant',
+    'Dragonborn Revenant',
+    'Dwarf Revenant',
+    'Elf Revenant',
+    'Firbolg Revenant',
+    'Genasi Revenant',
+    'Gnome Revenant',
+    'Goblin Revenant',
+    'Goliath Revenant',
+    'Half-Elf Revenant',
+    'Half-Orc Revenant',
+    'Halfling Revenant',
+    'Human Revenant',
+    'Kenku Revenant',
+    'Kor Revenant',
+    'Lizardfolk Revenant',
+    'Merfolk Revenant',
+    'Minator Revenant',
+    'Bugbear Revenant',
+    'Hobgoblin Revenant',
+    'Kobold Revenant',
+    'Orc Revenant',
+    'Yuan-ti Pureblood Revenant',
+    'Shifter Revenant',
+    'Tabaxi Revenant',
+    'Tiefling Revenant',
+    'Triton Revenant',
+    'Zendikar Vampire Revenant',
+    'Warforged Revenant'
+  ],
+  ['Warforged']
+]
+
+const characterTables = {
+  raceTable: raceTable
+}
+
+export default characterTables
